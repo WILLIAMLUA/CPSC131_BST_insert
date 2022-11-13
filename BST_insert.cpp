592144e3c46d9ae9ff9f69c8e92e08930ee2d6cb
WILLIAMLUA <98416548+WILLIAMLUA@users.noreply.github.com>
2022-11-12 22:06:01 -0800

    Update BST_insert.cpp

--- BST_insert.cpp
+++ BST_insert.cpp
@@ -10,21 +10,26 @@
  * };
  */
  
+
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
     //New node will be made in the tree if tree is empty
-        if(root==NULL){
-            return new TreeNode(val);
-        }
-        else{
-            // if val > current val insert it into left sub-tree
-            if(root->val>val) 
-                root->left=insertIntoBST(root->left,val);
-            else
-                // else val < current val insert it into right sub-tree
-                root->right=insertIntoBST(root->right,val);
-        }
+        if (!root) {
+            TreeNode *newNode = new TreeNode(val);
+            return newNode;
+		}
+        // if val < current val insert it into left sub-tree
+        if(val < root->val) root->left = insertIntoBST(root->left, val);
+        // else val > current val insert it into right sub-tree
+        else root->right = insertIntoBST(root->right, val);   
         return root;
     }
 };
+
+/**
+* If root==NULL then create a new node with value equal to val & return the node.
+* If val is less than root->val, then recursively call the function on the left subtree of the root & insert the node at a leaf node.
+* If val is greater than root->val, then recursively call the function on the right subtree & insert the node at the leaf node.
+* Finally return the root.
+*/
